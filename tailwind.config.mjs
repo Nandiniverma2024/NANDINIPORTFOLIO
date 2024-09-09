4f4e13f786d7ba887418b19f0b6909c9c5a825f5
Nandini <nandiniverma202021@gmail.com>
2024-09-09 16:26:15 +0530

    feat: created components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,65 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+                "fade-in-down": {
+                    "0%": {
+                        opacity: 0,
+                        transform: "translate3d(0, -100%, 0)",
+                    },
+                    "100%": {
+                        opacity: 1,
+                        transform: "translate3d(0, 0, 0)",
+                    },
+                },
+				"fade-in-up": {
+                    "0%": {
+                        opacity: 0,
+                        transform: "translate3d(0, 100%, 0)",
+                    },
+                    "100%": {
+                        opacity: 1,
+                        transform: "translate3d(0, 0, 0)",
+                    },
+                },
+				"slide-in-up": {
+                    "0%": {
+                        visibility: "visible",
+                        transform: "translate3d(0, 100%, 0)",
+                    },
+                    "100%": {
+                        transform: "translate3d(0, 0, 0)",
+                    },
+                },
+				"spinner-grow": {
+                    "0%": {
+                        transform: "scale(0)",
+                    },
+      
+                    "100%": {
+                        transform: "none",
+                        opacity: "1",
+                    },
+                },
+      		},
+      		animation: {
+
+                "fade-in-down": 'fade-in-down 0.25s ease-in 0.25s 1',
+				"fade-in-up": 'fade-in-up 1s ease-in-out 0.25s 1',
+				"slide-in-up": 'slide-in-up 1s ease-in-out 0.25s 1',
+				"spinner-grow": 'spinner-grow 1s ease-in-out 0.25s 1',
+
+			},
+			colors: {
+
+				"light_red":"#fce9d8",
+				"dark_red":"#ff6264",
+                "light_skyblue":"#80c4e1",
+                "light_gray":"#F0F1F1",
+
+			},
+		},
 	},
 	plugins: [],
 }
